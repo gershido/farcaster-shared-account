8a31ad149aab4b898dfe8011f444696c861b8840
Ido <gershido@gmail.com>
2024-05-22 14:47:21 +0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,7 @@ app.frame("/", (c) => {
   });
 });
 
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
 console.log(`Server is running on port ${port}`);
 
 devtools(app, { serveStatic });
