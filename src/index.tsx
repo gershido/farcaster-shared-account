74352ae6996f0c0312767812e72b119b6f2a6bed
Ido <gershido@gmail.com>
2024-05-23 09:11:02 +0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -20,20 +20,116 @@ app.use("/*", serveStatic({ root: "./public" }));
 app.frame("/", async (c) => {
   const { frameData } = c;
 
-  log.info(
-    `route: ${frameData?.url}, fid: ${frameData?.fid}, castId-fid: ${frameData?.castId.fid}, castId-hash: ${frameData?.castId.hash}, address: ${frameData?.address}`
-  );
+  log.info(`frame data: ${JSON.stringify(frameData)}`);
 
   return c.res({
+    action: "/select",
     image: (
-      <div style={{ color: "white", display: "flex", fontSize: 60 }}>Hello</div>
+      <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+        Select a shared account by its user name
+      </div>
     ),
-    intents: [<Button value="like">Like</Button>],
+    intents: [
+      <TextInput placeholder="Enter shared account user name" />,
+      <Button value="submit" action="/select">
+        Submit
+      </Button>,
+    ],
   });
 });
 
-app.frame("/shared-account/:user/:hash", async (c) => {
+app.frame("/select", async (c) => {
+  console.log("here");
   const { frameData } = c;
+
+  if (frameData === undefined || frameData.inputText === undefined) {
+    return c.res({
+      image: (
+        <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+          Error
+        </div>
+      ),
+      intents: [],
+    });
+  }
+
+  log.info(`frame data: ${JSON.stringify(frameData)}`);
+
+  const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY as string);
+
+  try {
+    const { result } = await client.lookupUserByUsername(frameData.inputText);
+    const { user: sharedAccount } = result;
+
+    log.info(`sharedAccount: ${JSON.stringify(sharedAccount)}`);
+
+    return c.res({
+      image: (
+        <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+          Shared Account
+        </div>
+      ),
+      intents: [<Button value="like">Like</Button>],
+    });
+  } catch (error) {
+    if (isApiErrorResponse(error)) {
+      log.info("API Error", error.response.data);
+    } else {
+      log.info("Generic Error", error);
+    }
+    return c.res({
+      image: (
+        <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+          Error
+        </div>
+      ),
+      intents: [],
+    });
+  }
+});
+
+/*
+app.frame("/shared-account/register", async (c) => {
+  const { frameData } = c;
+
+  const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY as string);
+
+  log.info(`frame data: ${JSON.stringify(frameData)}`);
+
+  try {
+    const { cast } = await client.fetchBulkUsers([frameData?.fid])
+
+    log.info(`cast details: ${JSON.stringify(cast)}`);
+
+    return c.res({
+      image: (
+        <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+          author username: {cast.author.username} author fid: {cast.author.fid}{" "}
+          cast hash: {cast.hash}
+        </div>
+      ),
+      intents: [<Button value="like">Like</Button>],
+    });
+  } catch (error) {
+    if (isApiErrorResponse(error)) {
+      log.info("API Error", error.response.data);
+    } else {
+      log.info("Generic Error", error);
+    }
+    return c.res({
+      image: (
+        <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+          Error
+        </div>
+      ),
+      intents: [],
+    });
+  }
+});
+*/
+app.frame("/shared-account/:name/act/:user/:hash", async (c) => {
+  const { frameData } = c;
+  const sharedAccountName = c.req.param("name");
   const user = c.req.param("user");
   const hash = c.req.param("hash");
   const url = `https://warpcast.com/${user}/${hash}`;
